d6cab2e5fda129e90231fd8f8d15e0328c3b2ffb
davidfeust <david.feust@gmail.com>
2021-03-26 04:50:10 +0300

    part b

--- Board.cpp
+++ Board.cpp
@@ -1,15 +1,29 @@
+#include <iostream>
+#include <string>
 #include "Board.hpp"
 
 namespace ariel {
 
-    Board::Board() : rows(0), cols(0), chars(nullptr) {}
+    Board::Board() : board(std::vector<std::vector<char>>('_')) {}
 
     Board::~Board() {
-        delete chars;
+//        delete board;
+//        for (int i = 0; i < board.size(); ++i) {
+//            delete board.at(i).;
+//        }
     }
 
-    void Board::post(uint row, uint column, Direction direction, const std::string& massage) {
-
+    void Board::post(uint row, uint column, Direction direction, const std::string &massage) {
+        uint index = 0;
+        if (direction == Direction::Horizontal) {
+            while (index < massage.size()) {
+                board.at(row).at(column + index) = massage.at(index++);
+            }
+        } else {
+            while (index < massage.size()) {
+                board.at(row + index).at(column) = massage.at(index++);
+            }
+        }
     }
 
     std::string Board::read(uint row, uint column, Direction direction, uint length) {
@@ -17,6 +31,10 @@ namespace ariel {
     }
 
     void Board::show() {
-
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board.at(i).size(); ++j) {
+                std::cout << board.at(i).at(j);
+            }
+        }
     }
 }
--- Board.hpp
+++ Board.hpp
@@ -1,17 +1,25 @@
 #include <string>
+#include <array>
+#include <vector>
 #include "Direction.hpp"
 
 namespace ariel {
     class Board {
-        char **chars;
-        uint rows;
-        uint cols;
+
+//        uint rows;
+//        uint cols;
+//        char **chars;
+//        std::array<std::array<char, rows>, cols> a;
+
+    private:
+        std::vector<std::vector<char>> board;
+
     public:
         Board();
 
         ~Board();
 
-        void post(uint row, uint column, Direction direction, const std::string& massage);
+        void post(uint row, uint column, Direction direction, const std::string &massage);
 
         std::string read(unsigned int row, unsigned int column, Direction direction, uint length);
 
